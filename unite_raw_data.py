9502fb474d2f012e2bed77e325422617b13e21d2
luis261 <luis.goerler@gmx.de>
2021-08-16 17:38:51 +0200

    Add poverty index calculation

--- unite_raw_data.py
+++ unite_raw_data.py
@@ -40,10 +40,6 @@ life_expectancy_data = pd.read_csv("data_raw/years_life_expectancy_at_birth.csv"
 life_expectancy_data.drop(labels = "Unnamed: 65", axis = 1, inplace = True)
 all_data.append(life_expectancy_data)
 
-print(life_expectancy_data.head())
-print("shape")
-print(life_expectancy_data.shape)
-
 transformed_data = {"Country and year": [], "Agriculture percentage of GDP": [], "GDP per capita (in USD 2010)": [], "Industry percentage of GDP": [], "Infant mortality per 1000 life births": [], "Primary education completion percentage": [], "Population": [], "Population density (people per sq. km)": [], "Service percentage of GDP": [], "Life expectancy (in years) at births": []}
 
 # print(list(transformed_data.keys())[0])
@@ -62,7 +58,6 @@ for k in range(all_data[0].shape[0]):
     transformed_data["Country and year"] += [country + " " + x for x in [str(x) for x in range(1960, 2021)]]
 
 rows_without_missing_values = pd.DataFrame(transformed_data).dropna().reset_index(drop = True)
-print(rows_without_missing_values)
 
 row_indices_to_keep = []
 values = rows_without_missing_values["Country and year"].tolist()
@@ -80,7 +75,23 @@ all_current_indices = list(rows_without_missing_values.index.values)
 row_indices_to_drop = [x for x in all_current_indices if x not in row_indices_to_keep]
 
 rows_without_missing_values = rows_without_missing_values.drop(row_indices_to_drop).reset_index(drop = True)
+
+# convert three parts of "our" HDI to values relative to the maximum value
+for i in [2, 5, 9]:
+    max_value = rows_without_missing_values.loc[rows_without_missing_values[list(transformed_data.keys())[i]].idxmax()][list(transformed_data.keys())[i]]
+    rows_without_missing_values[list(transformed_data.keys())[i]] = [x/max_value for x in list(rows_without_missing_values[list(transformed_data.keys())[i]])]
+
+calculated_poverty_index_values = []
+for index in rows_without_missing_values.index.values:
+    row = rows_without_missing_values.loc[index]
+    calculated_poverty_index_values.append(1 - (row[list(transformed_data.keys())[2]] * row[list(transformed_data.keys())[5]] * row[list(transformed_data.keys())[9]])**(1/3))
+rows_without_missing_values["Calculated poverty index"] = calculated_poverty_index_values
+
+for i in [2, 5, 9]:
+    del rows_without_missing_values[list(transformed_data.keys())[i]]
+
 print(rows_without_missing_values)
+print(rows_without_missing_values.loc[0])
 
 """
 row0 = []
@@ -113,6 +124,6 @@ print(data["Unnamed: 65"].isna().sum())
 
 """
 TODO next steps:
-- generate new dataframe which does not contain the individual components of our "HDI" but the actual values
 - generate new dataframe which does not contain absolute values, but contains the changes instead
+- correlation analysis
 """
