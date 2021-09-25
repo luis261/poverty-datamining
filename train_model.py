499d7a8b48e7c6c4fc6794417a038c1e2d64e422
luis261 <luis.goerler@gmx.de>
2021-09-25 17:51:22 +0200

    Remove unnecessary files

--- train_model.py
+++ train_model.py
@@ -9,21 +9,19 @@ def main():
     data = pd.read_pickle("final_data.pkl").drop(columns = ["Country and timespan"])
     print(data)
 
-    # TODO where is the right place for this code?
+    print("Percentage of right guesses if you were to guess that the poverty headcount ratio increased more than the median change every year:")
     i = 0
     j = 0
     for x in data["POV"]:
         i += 1
         if x == 1.0:
             j += 1
-
     print(j/i)
 
     X = data.drop(columns = ["POV NXT"])
     y = data["POV NXT"]
-
     scores = cross_val_score(RandomForestClassifier(n_estimators = 10000), X, y, cv = 5)
-    print(scores)
+    print("Percentage of right predictions of the model:")
     print(np.mean(scores))
 
 
